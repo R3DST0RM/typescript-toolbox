813586542a684e25147d5f7620d4978734b15099
R3DST0RM <dominik.schwarzbauer@gmail.com>
2019-11-13 21:49:47 +0100

    Enhanced test coverage for waitFor function

--- src/timeout-utils/__test__/waitFor.test.ts
+++ src/timeout-utils/__test__/waitFor.test.ts
@@ -9,4 +9,12 @@ describe("waitFor function", () => {
             return result !== 0;
         });
     });
+
+    it("rejects if validator does not return true in time", done => {
+        waitFor(null, () => false, {interval: 20, timeout: 50})
+            .then(() => {
+                throw Error("Should not enter here");
+            })
+            .catch(() => done());
+    });
 });
