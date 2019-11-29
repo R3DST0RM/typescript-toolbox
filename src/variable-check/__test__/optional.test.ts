ab60fa97986b9bffa299dcc7e9b72b7cf2edd9cf
R3DST0RM <dominik.schwarzbauer@gmail.com>
2019-11-29 14:51:15 +0100

    Create optional.test.ts
    Added tests for Optional

---
+++ src/variable-check/__test__/optional.test.ts
@@ -0,0 +1,38 @@
+import { Optional } from "../optional";
+
+describe("Optional", () => {
+    it("orElse on an empty optional returns specified default value", () => {
+        const emptyOptional = Optional.empty();
+
+        expect(emptyOptional.orElse(10)).toBe(10);
+    });
+
+    it("parse null or undefined into optional throws an error", () => {
+        expect(() => Optional.of(null)).toThrowError("Provided value can not be null or unassigned");
+    });
+
+    it("parse non null value into optional succeeds without error", () => {
+        expect(() => Optional.of(1)).not.toThrowError("Provided value can not be null or unassigned");
+    });
+
+    it("parse null or undefined using ofNullable does not throw", () => {
+        expect(() => Optional.ofNullable(null)).not.toThrowError("Provided value can not be null or unassigned");
+        expect(() => Optional.ofNullable(undefined)).not.toThrowError("Provided value can not be null or unassigned");
+    });
+
+    it("orElse retrieves the value of Optional if not null", () => {
+        const emptyOptional = Optional.empty();
+        const nonEmptyOptional = Optional.of(5);
+
+        expect(emptyOptional.orElse(1)).toBe(1);
+        expect(nonEmptyOptional.orElse(0)).toBe(5);
+    });
+
+    it("orElseGet retrieves the value of Optional if not null and default if null or undefined", () => {
+        const emptyOptional = Optional.empty();
+        const nonEmptyOptional = Optional.of(5);
+
+        expect(emptyOptional.orElseGet(() => 1)).toBe(1);
+        expect(nonEmptyOptional.orElseGet(() => 0)).toBe(5);
+    });
+});
